bce4994b9a1d8e2bc4dbf009d76da97f445e2f38
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2016-10-08 11:32:56 +0200

    Slightly more dreamy appearance

--- app.elm
+++ app.elm
@@ -1,16 +1,57 @@
-import Html exposing (div, text)
-import Html.Attributes exposing (style)
+module Main exposing (..)
+
+import Html exposing (div, img, text)
+import Html.Attributes exposing (style, src)
+
 
 main : Html.Html msg
 main =
-  div [ boxStyle, mainStyle, largeText ] [ text "Your dream appearance." ]
+    div [ boxStyle, mainStyle, largeText ]
+        [ inlineDiv [ text "Everyting that matters" ]
+        , arrow
+        , inlineDiv [ img [ src "elm-logo.png" ] [] ]
+        , arrow
+        , inlineDiv [ text "Your dream appearance." ]
+        ]
+
+
+arrow =
+    div (arrowStyle :: inlineDivStyles) [ text "=>" ]
+
+
+inlineDiv elements =
+    div inlineDivStyles elements
+
+
+
+----- STYLING -------
+
+
+mainStyle =
+    style [ ( "font-family", "Helvetica" ) ]
 
 
 boxStyle =
-  style [ ("padding", "30px") ]
+    style [ ( "padding", "30px" ) ]
 
-mainStyle =
-  style [ ("font-family", "Helvetica") ]
+
+arrowStyle =
+    style
+        [ ( "font-size", "64px" )
+        , ( "color", "#474E63" )
+        ]
+
 
 largeText =
-  style [ ("font-size", "24px") ]
+    style [ ( "font-size", "24px" ) ]
+
+
+inline =
+    style
+        [ ( "display", "inline-block" )
+        , ( "vertical-align", "middle" )
+        ]
+
+
+inlineDivStyles =
+    [ boxStyle, inline ]
