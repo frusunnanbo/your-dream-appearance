f808871da1b9cb42229a34d54313a5d8028e8f33
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2016-10-18 17:29:09 +0200

    Moved styling out of app logic

--- App.elm
+++ App.elm
@@ -2,10 +2,10 @@ module Main exposing (main)
 
 import Html exposing (Html, button, div, img, text)
 import Html.App exposing (program)
-import Html.Attributes exposing (src)
+import Html.Attributes exposing (id, src, class)
 import Html.Events exposing (onClick)
 import Svg exposing (svg, circle, text')
-import Svg.Attributes exposing (..)
+import Svg.Attributes exposing (x, y, cx, cy, r, fill, fontSize, textAnchor, alignmentBaseline, width, height)
 import Time exposing (every, millisecond)
 
 
@@ -66,10 +66,10 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-    div [ boxStyle, mainStyle, largeText ]
+    div [ id "main" ]
         [ everyThingThatMatters
         , arrow
-        , inlineDiv [ img [ src "elm-logo.png" ] [] ]
+        , div [] [ img [ src "elm-logo.png" ] [] ]
         , arrow
         , yourDreamAppearance model
         ]
@@ -77,19 +77,19 @@ view model =
 
 everyThingThatMatters : Html Msg
 everyThingThatMatters =
-    inlineDiv
-        [ button [ buttonStyle, onClick Decrement ] [ text "-" ]
-        , button [ buttonStyle, onClick Increment ] [ text "+" ]
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , button [ onClick Increment ] [ text "+" ]
         ]
 
 
 yourDreamAppearance : Model -> Html Msg
 yourDreamAppearance model =
-    div (boundedBox :: [ inline ])
+    div [ class "dream" ]
         [ if model < 0 then
-            img [ src "alf.jpg", Html.Attributes.width 200 ] []
+            img [ src "alf.jpg" ] []
           else if model > 100 then
-            img [ src "pia.jpg", Html.Attributes.width 200 ] []
+            img [ src "pia.jpg" ] []
           else
             theAnimation model
         ]
@@ -113,74 +113,11 @@ theAnimation model =
 
 arrow : Html Msg
 arrow =
-    div (arrowStyle :: inlineDivStyles) [ text "=>" ]
-
-
-inlineDiv : List (Html Msg) -> Html Msg
-inlineDiv elements =
-    div inlineDivStyles elements
+    div [ class "arrow" ] [ text "=>" ]
 
 
 
------ STYLING
-
-
-mainStyle =
-    Html.Attributes.style
-        [ ( "font-family", "Helvetica" )
-        , ( "color", elmDarkblue )
-        ]
-
-
-boxStyle =
-    Html.Attributes.style [ ( "padding", "30px" ) ]
-
-
-buttonStyle =
-    Html.Attributes.style
-        [ ( "font-size", "24px" )
-        , ( "background-color", "#7CD12A" )
-        , ( "border", "none" )
-        , ( "border-radius", "4px" )
-        , ( "min-height", "45px" )
-        , ( "min-width", "45px" )
-        , ( "margin", "5px" )
-        ]
-
-
-arrowStyle =
-    Html.Attributes.style
-        [ ( "font-size", "64px" ) ]
-
-
-boundedBox =
-    Html.Attributes.style
-        [ ( "height", "200px" )
-        , ( "border", "4px solid " ++ elmLightblue )
-        , ( "border-radius", "4px" )
-        ]
-
-
-appearanceTextStyle =
-    Html.Attributes.style
-        [ ( "font-size", "128px" )
-        , ( "padding", "30px 60px 30px 60px" )
-        ]
-
-
-largeText =
-    Html.Attributes.style [ ( "font-size", "24px" ) ]
-
-
-inline =
-    Html.Attributes.style
-        [ ( "display", "inline-block" )
-        , ( "vertical-align", "middle" )
-        ]
-
-
-inlineDivStyles =
-    [ boxStyle, inline ]
+----- Colors
 
 
 elmDarkblue =
